0cae7ce6857da2f1e10430629e6476f18cfdb0df
githubnguyendang <73978541+githubnguyendang@users.noreply.github.com>
2023-04-20 11:41:59 +0700

    #Update filter form kin Construction

--- src/pages/cong-trinh/nuoc-mat/index.tsx
+++ src/pages/cong-trinh/nuoc-mat/index.tsx
@@ -1,6 +1,5 @@
 // ** MUI Imports
-import { Box, Button, Typography } from '@mui/material';
-import Grid from '@mui/material/Grid'
+import { Grid, Box, Button, Typography, Autocomplete, TextField } from '@mui/material';
 import { useEffect } from 'react'
 
 // ** Icons Imports
@@ -10,18 +9,17 @@ import SearchIcon from '@mui/icons-material/Search';
 
 // ** Components Imports
 import ConstructionMap from 'src/views/construction'
-import AutoComplete from 'src/views/construction/Autocomplete';
 import TableList from 'src/views/construction/TableList';
 
 const complete1 = [
-  {label: "Khóa 1"},
-  {label: "Khóa 2"},
-  {label: "Khóa 3"},
+  {title: "Khóa 1"},
+  {title: "Khóa 2"},
+  {title: "Khóa 3"},
 ];
 const complete2 = [
-  {label: "Đợt 1"},
-  {label: "Đợt 2"},
-  {label: "Đợt 3"},
+  {title: "Đợt 1"},
+  {title: "Đợt 2"},
+  {title: "Đợt 3"},
 ];
 
 const SurfaceWater = () => {
@@ -34,8 +32,8 @@ const SurfaceWater = () => {
        <Grid item xs={12} sm={12} md={12} sx={{height:'60vh'}}>
           <ConstructionMap />
        </Grid>
-       <Grid item xs={12} sm={12} md={12} className='total_contruction'>
-          <Box>
+       <Grid item xs={12} sm={12} md={12} className='_row _justifyContentBetween' >
+          <Box className='_count'>
             <Typography>
               Tổng số công trình KTSDN  mặt: 132
             </Typography>
@@ -43,13 +41,41 @@ const SurfaceWater = () => {
               Số công trình đã cấp phép: 132
             </Typography>
           </Box>
-          <Box className='contruction_search'>
-            <AutoComplete options={complete1} label={"Chọn loại CT"}/>
-            <AutoComplete options={complete2} label={"Chọn cơ quan CP"}/>
-            <Button startIcon={<FilterAltIcon/>} variant="outlined">Bộ lọc</Button>
-            <Button startIcon={<SearchIcon/>} variant="outlined">Tìm kiếm</Button>
-            <Button startIcon={<SearchIcon/>} variant="outlined">Xuất excel</Button>
-            <Button startIcon={<AddIcon/>} variant="outlined">Thêm mới</Button>
+          <Box className='_search'>
+            <Box>
+              <Autocomplete  size="small" options={complete1} getOptionLabel={(option) => option.title} renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    variant="outlined"
+                    label="Chọn loại hình CP"
+                    placeholder=""
+                  />
+                )}
+              />
+            </Box>
+            <Box>
+              <Autocomplete size="small" options={complete2} getOptionLabel={(option) => option.title} renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    variant="outlined"
+                    label="Chọn cơ quan CP"
+                    placeholder=""
+                  />
+                )}
+              />
+            </Box>
+            <Box>
+              <Button size='small' startIcon={<FilterAltIcon/>} variant="outlined">Bộ lọc</Button>
+            </Box>
+            <Box>
+              <Button size='small' startIcon={<SearchIcon/>} variant="outlined">Tìm kiếm</Button>
+            </Box>
+            <Box>
+              <Button size='small' startIcon={<SearchIcon/>} variant="outlined">Xuất excel</Button>
+            </Box>
+            <Box>
+              <Button size='small' startIcon={<AddIcon/>} variant="outlined">Thêm mới</Button>
+            </Box>
           </Box>
        </Grid>
        <Grid item xs={12} sm={12} md={12}>
