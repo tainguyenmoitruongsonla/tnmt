8f2676c36e9bb623d77e30c24b623ab74f45120b
trungquan21 <89087554+trungquan21@users.noreply.github.com>
2023-08-07 16:57:48 +0700

    #report-form

--- src/pages/bao-cao-bieu-mau/index.tsx
+++ src/pages/bao-cao-bieu-mau/index.tsx
@@ -3,9 +3,33 @@ import Grid from '@mui/material/Grid'
 import React from 'react'
 import { useEffect } from 'react'
 import Bieumau1 from 'src/views/report-form/Bieumau1'
+import Bieumau10 from 'src/views/report-form/Bieumau10'
+import Bieumau11 from 'src/views/report-form/Bieumau11'
+import Bieumau12 from 'src/views/report-form/Bieumau12'
+import Bieumau13 from 'src/views/report-form/Bieumau13'
+import Bieumau14 from 'src/views/report-form/Bieumau14'
+import Bieumau15 from 'src/views/report-form/Bieumau15'
+import Bieumau16 from 'src/views/report-form/Bieumau16'
+import Bieumau17 from 'src/views/report-form/Bieumau17'
+import Bieumau18A from 'src/views/report-form/Bieumau18A'
+import Bieumau18B from 'src/views/report-form/Bieumau18B'
+import Bieumau19 from 'src/views/report-form/Bieumau19'
 import Bieumau2 from 'src/views/report-form/Bieumau2'
+import Bieumau20 from 'src/views/report-form/Bieumau20'
+import Bieumau21 from 'src/views/report-form/Bieumau21'
+import Bieumau22 from 'src/views/report-form/Bieumau22'
+import Bieumau23 from 'src/views/report-form/Bieumau23'
+import Bieumau24 from 'src/views/report-form/Bieumau24'
+import Bieumau25A from 'src/views/report-form/Bieumau25A'
+import Bieumau25B from 'src/views/report-form/Bieumau25B'
 import Bieumau3 from 'src/views/report-form/Bieumau3'
 import Bieumau4 from 'src/views/report-form/Bieumau4'
+import Bieumau5 from 'src/views/report-form/Bieumau5'
+import Bieumau6A from 'src/views/report-form/Bieumau6A'
+import Bieumau6B from 'src/views/report-form/Bieumau6B'
+import Bieumau7 from 'src/views/report-form/Bieumau7'
+import Bieumau8 from 'src/views/report-form/Bieumau8'
+import Bieumau9 from 'src/views/report-form/Bieumau9'
 
 // ** Icons Imports
 const Report = () => {
@@ -26,6 +50,78 @@ const Report = () => {
       </Grid>
       <Grid item xs={3}>
       <Bieumau4 />
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau5 />
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau6A />
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau6B />
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau7 />
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau8/>
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau9 />
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau10/>
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau11 />
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau12 />
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau13 />
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau14 />
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau15 />
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau16 />
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau17 />
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau18A />
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau18B/>
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau19 />
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau20 />
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau21 />
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau22 />
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau23 />
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau24 />
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau25A/>
+      </Grid>
+      <Grid item xs={3}>
+      <Bieumau25B />
       </Grid>
     </Grid>
   )
