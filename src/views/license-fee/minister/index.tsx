f326b6bbba7d1146dd3d0f2c5bb520bf6096d1a2
githubnguyendang <73978541+githubnguyendang@users.noreply.github.com>
2023-08-17 09:31:25 +0700

    reup licenseffee

---
+++ src/views/license-fee/minister/index.tsx
@@ -0,0 +1,137 @@
+import { useState, useEffect } from 'react'
+
+// ** MUI Imports
+import { Grid, Typography, Paper, Box, Tooltip, IconButton } from '@mui/material'
+
+// ** Icons Imports
+import fetchData from 'src/api/fetch';
+import { useLoadingContext } from 'src/@core/theme/loading-provider';
+import FormatDate from 'src/@core/components/format-date';
+import postData from 'src/api/post'
+import FormLicenseFee from 'src/views/license-fee/form'
+import DataGridComponent, { columnFillters } from 'src/@core/components/data-grid'
+import { GridColDef } from '@mui/x-data-grid'
+import { Delete } from 'mdi-material-ui';
+import ShowFilePDF from 'src/@core/components/show-file-pdf';
+
+const LicenseMinister = () => {
+
+  const [postSuccess, setPostSuccess] = useState(false);
+  const { showLoading, hideLoading } = useLoadingContext();
+  const [loading, setLoading] = useState(false)
+  if (loading == true) {
+    showLoading();
+  } else {
+    hideLoading();
+  }
+  const handlePostSuccess = () => {
+    setPostSuccess(prevState => !prevState);
+  };
+
+  const [resData, setResData] = useState([]);
+
+  const columns: GridColDef[] = [
+    { field: 'id', headerClassName: 'tableHead', headerAlign: 'center', flex: 1, headerName: 'ID', minWidth: 90 },
+    { field: 'licenseFeeNumber', headerClassName: 'tableHead', headerAlign: 'center', flex: 1, headerName: 'Quyết định cấp quyền', renderCell: (data: any) => (<ShowFilePDF name={data.row.licenseFeeNumber} src={`/pdf/licenseFees/` + data.row.licensingAuthorities + `/` + data.row.filePDF} />) },
+    { field: 'signDate', headerClassName: 'tableHead', headerAlign: 'center', flex: 1, headerName: 'Ngày ký', renderCell: (data: any) => FormatDate(data.row.signDate) },
+    { field: 'supplementLicenseFee', headerClassName: 'tableHead', headerAlign: 'center', flex: 1, headerName: 'Quyết định bổ sung', renderCell: (data: any) => (<ShowFilePDF name={data.row.supplementLicenseFee?.licenseFeeNumber} src={`/pdf/licenseFees/` + data.row.supplementLicenseFee?.licensingAuthorities + `/` + data.row.supplementLicenseFee?.filePDF} />) },
+    { field: 'totalMoney', headerClassName: 'tableHead', headerAlign: 'center', flex: 1, headerName: 'Tổng số tiền cấp quyền(VNĐ)' },
+    { field: 'description', headerClassName: 'tableHead', headerAlign: 'center', flex: 1, headerName: 'Ghi chú' },
+    { field: 'LicenseNumber', headerClassName: 'tableHead', headerAlign: 'center', flex: 1, headerName: 'Giấy phép' },
+    { field: 'ConstructionName', headerClassName: 'tableHead', headerAlign: 'center', flex: 1, headerName: 'Công trình' },
+
+    //Action
+    {
+      field: 'actions', headerClassName: 'tableHead', headerAlign: 'center', headerName: '#', minWidth: 120, sortable: false,
+      renderCell: (data) => (
+        <Box>
+          <Tooltip title="Chỉnh sửa tiền cấp quyền">
+            <IconButton>
+              <FormLicenseFee isEdit={true} data={data.row} />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Xóa tiền cấp quyền">
+            <IconButton onClick={() => DeleteLicense(data)}>
+              <Delete className='tableActionBtn deleteBtn' />
+            </IconButton>
+          </Tooltip>
+        </Box>
+      )
+    },
+  ]
+
+  const columnFillter: columnFillters[] = [
+    {
+      label: 'Số QĐ',
+      value: 'licenseFeeNumber',
+      type: 'text',
+    },
+    {
+      label: 'Năm',
+      value: 'signDate',
+      type: 'dateRange',
+      options: [
+        { label: '2021', value: 2021 },
+        { label: '2022', value: 2022 },
+        { label: '2023', value: 2023 },
+      ],
+    }
+  ]
+
+  useEffect(() => {
+    const getData = async () => {
+      setLoading(true);
+      try {
+        const data = await fetchData('LicenseFee/list');
+        setResData(data);
+      } catch (error) {
+        setResData([]);
+      } finally {
+        setLoading(false);
+      }
+    };
+    getData();
+  }, [postSuccess]);
+
+
+  const DeleteLicense = async (row: any) => {
+    console.log(row)
+    try {
+      await postData('LicenseFee/delete', row)
+    } catch (error) {
+      console.error(error)
+    }
+    setPostSuccess(true);
+    handlePostSuccess();
+  }
+
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12} sm={12} md={12} className='text-center'>
+        <Typography className='font-weight-bold ' variant='h4'>
+          Thống kê
+        </Typography>
+        <Typography className='font-weight-bold ' variant='h6'>
+          KẾT QUẢ THU TIỀN CẤP QUYỀN KHAI THÁC SỬ DỤNG CỦA BỘ
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={12} md={12}>
+        <Typography >Tổng số tiền cấp quyền: 426.293.061.000₫</Typography>
+      </Grid>
+      <Grid item xs={12} sm={12} md={12}>
+        <Paper elevation={3} sx={{ p: 0, height: '100%' }}>
+          <DataGridComponent
+            rows={resData}
+            columns={columns}
+            columnFillter={columnFillter}
+            actions={
+              <FormLicenseFee setPostSuccess={handlePostSuccess} isEdit={false} />
+            }
+          />
+        </Paper>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default LicenseMinister
---
+++ src/views/license-fee/province/index.tsx
@@ -0,0 +1,177 @@
+import { useState, useEffect } from 'react'
+
+// ** MUI Imports
+import { Grid, Box, IconButton, Tooltip, Button, Typography, TextField, Autocomplete } from '@mui/material'
+
+// ** Icons Imports
+import { EditNote, Delete } from '@mui/icons-material'
+
+import TableComponent from 'src/@core/components/table'
+import licenseFeeMinister from 'src/api/licensefee/bocapquyen'
+import FormatDate from 'src/@core/components/format-date';
+
+
+// id of columnsTable is parameter to bind ex: get LicseFk.BasinId: id: 'License_Fk.BasinId'
+const complete2 = [{ title: 'Đợt 1' }, { title: 'Đợt 2' }, { title: 'Đợt 3' }]
+const columnsTable = [
+  {
+    id: 'stt',
+    label: 'STT'
+  },
+  {
+    id: 'LicenseFeeNumber',
+    label: 'Quyết định cấp quyền'
+  },
+  {
+    id: 'SignDate',
+    label: 'Ngày ký', format: (value: any) => FormatDate(value)
+  },
+  {
+    id: '#',
+    label: 'Quyết định bổ sung'
+  },
+  {
+    id: 'TotalMoney',
+    label: 'Tổng số tiền cấp quyền(VNĐ)'
+  },
+  {
+    id: '#',
+    label: 'Ghi chú'
+  },
+  {
+    id: 'LicenseNumber',
+    label: 'Giấy phép'
+  },
+  {
+    id: 'ConstructionName',
+    label: 'Công trình'
+  },
+  { id: 'actions', label: 'Thao tác' }
+]
+
+const LicenseProvince = () => {
+  const [data, setData] = useState<any[]>([])
+  const [columns, setColumns] = useState<any[]>([])
+  const [TypeOfConsId, setTypeOfConsId] = useState([1])
+  const handleChange = (e: any) => {
+    const val = e == undefined || e == null ? 1 : e.value
+    setTypeOfConsId(val)
+  }
+
+  // const fetchData = async () => {
+  //   try {
+  //     const response = await fetch('http://tnnsl.loc/api/Construction/list?BasinId=0&CommuneId=0&DistrictId=0&Keyword=&LicenseId=-1&LicensingAuthorities=-1&PageIndex=1&PageSize=0&ProvinceId=0&StartDate=-1&Status=true&TypeOfConstructionId=1'); // Thay đổi URL API tùy thuộc vào nguồn dữ liệu của bạn
+  //     const jsonData = await response.json();
+  //     console.log(jsonData.ListData)
+  //     setData(jsonData.ListData);
+  //   } catch (error) {
+  //     console.error('Error fetching data:', error);
+  //   }
+  // };
+
+  useEffect(() => {
+    setData(licenseFeeMinister)
+    setColumns(columnsTable)
+
+    // fetchData();
+  }, [])
+
+  const EditLicense = (row: any) => {
+    console.log('Edit: ' + row.LicenseNumber)
+  }
+
+  const DeleteLicense = (row: any) => {
+    console.log('Delete: ' + row.LicenseNumber)
+  }
+
+  return (
+    <Grid container spacing={3}>
+      <Grid item xs={12} sm={12} md={12} className='text-center'>
+        <Typography className='font-weight-bold ' variant='h4'>
+          Thống kê
+        </Typography>
+        <Typography className='font-weight-bold ' variant='h6'>
+          KẾT QUẢ THU TIỀN CẤP QUYỀN KHAI THÁC SỬ DỤNG CỦA TỈNH
+        </Typography>
+      </Grid>
+      <Grid item xs={12} sm={5} md={5}>
+        <Typography >Tổng số công trình KTSDN mặt: 426.293.061.000₫</Typography>
+      </Grid>
+      <Grid item xs={12} sm={7} md={7}>
+        <Grid container direction='row' justifyContent='flex-end' alignItems='center' spacing={3}>
+          <Grid item xs={12} sm={4} md={4}>
+            <TextField label='Nhập số quyết định' size='small' fullWidth />
+          </Grid>
+
+          <Grid item xs={12} sm={2} md={2}>
+            <Autocomplete
+              fullWidth
+              onChange={(e: any, v: any) => handleChange(v)}
+              size='small'
+              options={complete2}
+              getOptionLabel={(option: any) => option.title}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  variant='standard'
+                  fullWidth
+                  label='Từ:1998'
+                />
+              )}
+            />
+          </Grid>
+          <Grid item xs={12} sm={2} md={2}>
+            <Autocomplete
+              fullWidth
+              onChange={(e: any, v: any) => handleChange(v)}
+              size='small'
+              options={complete2}
+              getOptionLabel={(option: any) => option.title}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  variant='standard'
+                  fullWidth
+                  label='Đến:2023'
+                />
+              )}
+            />
+          </Grid>
+          <Grid item xs={12} sm={2} md={2}>
+            <Button size='small' fullWidth variant='outlined'>
+              Tìm kiếm
+            </Button>
+          </Grid>
+          <Grid item xs={12} sm={2} md={2}>
+            <Button size='small' fullWidth variant='outlined'>
+              Thêm mới
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid item xs={12} sm={12} md={12}>
+        <TableComponent
+          columns={columns}
+          data={data}
+          show={TypeOfConsId}
+          actions={(row: any) => (
+            <Box>
+              <Tooltip title='Chỉnh sửa giấy phép'>
+                <IconButton onClick={() => EditLicense(row)}>
+                  <EditNote className='tableActionBtn' />
+                </IconButton>
+              </Tooltip>
+              <Tooltip title='Xóa giấy phép'>
+                <IconButton onClick={() => DeleteLicense(row)}>
+                  <Delete className='tableActionBtn deleteBtn' />
+                </IconButton>
+              </Tooltip>
+            </Box>
+          )}
+        />
+      </Grid>
+    </Grid>
+  )
+}
+
+export default LicenseProvince
