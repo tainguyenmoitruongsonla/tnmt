3407a057e920d03aad1edc8e5f30530880c807d6
trungquan21 <89087554+trungquan21@users.noreply.github.com>
2023-10-17 15:56:14 +0700

    #call api bieu mau 1 den 10

--- src/views/report-form/Bieumau5.tsx
+++ src/views/report-form/Bieumau5.tsx
@@ -17,8 +17,54 @@ import DownloadIcon from '@mui/icons-material/Download'
 import DialogControlFullScreen from 'src/@core/components/dialog-control-full-screen'
 import HeaderReport from './HeaderReport'
 import FooterReport from './FooterReport'
+import { getData } from 'src/api/axios'
+import { useEffect, useState } from 'react'
+import BoxLoading from 'src/@core/components/box-loading'
+import CreateReport2 from './Bieumau2/CreateReport2'
+import DeleteData from 'src/@core/components/delete-data'
 
+interface Report5 {
+  id: number
+  tenTram: string
+  luuLuongThang1: number
+  luuLuongThang2: number
+  luuLuongThang3: number
+  luuLuongThang4: number
+  luuLuongThang5: number
+  luuLuongThang6: number
+  luuLuongThang7: number
+  luuLuongThang8: number
+  luuLuongThang9: number
+  luuLuongThang10: number
+  luuLuongThang11: number
+  luuLuongThang12: number
+  ghiChu: string
+}
 const FormContruction = () => {
+  const [data, setData] = useState<Report5[]>([])
+  const [loading, setLoading] = useState(false)
+  const [postSuccess, setPostSuccess] = useState(false)
+  const handlePostSuccess = () => {
+    setPostSuccess(prevState => !prevState)
+  }
+  useEffect(() => {
+    async function getDataReport1() {
+      setLoading(true)
+      await getData('BieuMauSoNam/danhsach')
+        .then(data => {
+          setData(data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+        .finally(() => {
+          setLoading(false)
+        })
+    }
+
+    getDataReport1()
+  }, [postSuccess])
+
   return (
     <Paper sx={{ p: 8 }}>
       {/* dautrang */}
@@ -32,106 +78,123 @@ const FormContruction = () => {
           </IconButton>
         </Grid>
       </Grid>
-      <HeaderReport/>
+      <HeaderReport />
 
       <Grid className='_text_center'>
         <Typography className='font-weight-bold ' variant='h4'>
           BÁO CÁO
         </Typography>
         <Typography className='font-weight-bold ' variant='h6'>
-         Dòng chảy trung bình tháng năm trong kỳ báo cáo
+          Dòng chảy trung bình tháng năm trong kỳ báo cáo
         </Typography>
         <Typography className='font-weight-bold ' variant='h6'>
           (Kỳ báo cáo: <TextField size='small' sx={{ width: '50px' }}></TextField>)
         </Typography>
       </Grid>
+      {loading ? (
+        <BoxLoading />
+      ) : (
+        <Grid className='_text_center' sx={{ mt: 3 }}>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+              <TableHead className='tableHead'>
+                <TableRow>
+                  <TableCell size='small' align='center' rowSpan={2}>
+                    STT
+                  </TableCell>
+                  <TableCell size='small' align='center' rowSpan={2}>
+                    Tên trạm
+                  </TableCell>
 
-      <Grid className='_text_center' sx={{ mt: 3 }}>
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label='simple table'>
-            <TableHead className='tableHead'>
-              <TableRow>
-                <TableCell size='small' align='center' rowSpan={2}>
-                  STT
-                </TableCell>
-                <TableCell size='small' align='center' rowSpan={2}>
-                  Tên trạm
-                </TableCell>
-                
-                <TableCell size='small' align='center' colSpan={12}>
-                 Lưu lượng(m3/s)
-                </TableCell>
-                <TableCell size='small' align='center' rowSpan={2}>
-                  Lưu lượng bình quân(m3/s)
-                </TableCell>
+                  <TableCell size='small' align='center' colSpan={12}>
+                    Lưu lượng(m3/s)
+                  </TableCell>
+                  <TableCell size='small' align='center' rowSpan={2}>
+                    Lưu lượng bình quân(m3/s)
+                  </TableCell>
+                  <TableCell size='small' align='center' rowSpan={2}>
+                    Thao tác
+                  </TableCell>
+                </TableRow>
+
+                <TableRow>
+                  <TableCell size='small' align='center'>
+                    I
+                  </TableCell>
+                  <TableCell size='small' align='center'>
+                    II
+                  </TableCell>
+                  <TableCell size='small' align='center'>
+                    III
+                  </TableCell>
+
+                  <TableCell size='small' align='center'>
+                    IV
+                  </TableCell>
+                  <TableCell size='small' align='center'>
+                    V
+                  </TableCell>
+                  <TableCell size='small' align='center'>
+                    VI
+                  </TableCell>
+
+                  <TableCell size='small' align='center'>
+                    VII
+                  </TableCell>
+                  <TableCell size='small' align='center'>
+                    VIII
+                  </TableCell>
+                  <TableCell size='small' align='center'>
+                    IX
+                  </TableCell>
+
+                  <TableCell size='small' align='center'>
+                    X
+                  </TableCell>
+                  <TableCell size='small' align='center'>
+                    XI
+                  </TableCell>
+                  <TableCell size='small' align='center'>
+                    XII
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+
+              <TableBody className='tableBody'>
+              {data.map((item, index) => (
+              <TableRow key={item.id}>
+                  <TableCell align='center' className="size='small' align-middle font-13">{index + 1}</TableCell>
+                  <TableCell align='center' className="size='small' align-middle font-13">{item.tenTram}</TableCell>
+                  <TableCell align='center' className="size='small' align-middle font-13">{item.luuLuongThang1}</TableCell>
+                  <TableCell align='center' className="size='small' align-middle font-13">{item.luuLuongThang2}</TableCell>
+                  <TableCell align='center' className="size='small' align-middle font-13">{item.luuLuongThang3}</TableCell>
+                  <TableCell align='center' className="size='small' align-middle font-13">{item.luuLuongThang4}</TableCell>
+                  <TableCell align='center' className="size='small' align-middle font-13">{item.luuLuongThang5}</TableCell>
+                  <TableCell align='center' className="size='small' align-middle font-13">{item.luuLuongThang6}</TableCell>
+                  <TableCell align='center' className="size='small' align-middle font-13">{item.luuLuongThang7}</TableCell>
+                  <TableCell align='center' className="size='small' align-middle font-13">{item.luuLuongThang8}</TableCell>
+                  <TableCell align='center' className="size='small' align-middle font-13">{item.luuLuongThang9}</TableCell>
+                  <TableCell align='center' className="size='small' align-middle font-13">{item.luuLuongThang10}</TableCell>
+                  <TableCell align='center' className="size='small' align-middle font-13">{item.luuLuongThang11}</TableCell>
+                  <TableCell align='center' className="size='small' align-middle font-13">{item.luuLuongThang12}</TableCell>
+                  <TableCell align='center' className="size='small' align-middle font-13">{item.ghiChu}</TableCell>
+                  <TableCell align='center' className="  size='small' align-middle font-13">
+                    <Box>
+                    <CreateReport2 isEdit={true} data={item} setPostSuccess={handlePostSuccess} />
+                    <DeleteData url={'BieuMauSoHai'} data={item} setPostSuccess={handlePostSuccess} />
+
+                </Box>
+                    </TableCell>
               </TableRow>
-
-              <TableRow>
-                <TableCell size='small' align='center'>
-                  I
-                </TableCell>
-                <TableCell size='small' align='center'>
-                  II
-                </TableCell>
-                <TableCell size='small' align='center'>
-                  III
-                </TableCell>
-
-                <TableCell size='small' align='center'>
-                  IV
-                </TableCell>
-                <TableCell size='small' align='center'>
-                  V
-                </TableCell>
-                <TableCell size='small' align='center'>
-                  VI
-                </TableCell>
-
-                <TableCell size='small' align='center'>
-                  VII
-                </TableCell>
-                <TableCell size='small' align='center'>
-                  VIII
-                </TableCell>
-                <TableCell size='small' align='center'>
-                  IX
-                </TableCell>
-
-                <TableCell size='small' align='center'>
-                  X
-                </TableCell>
-                <TableCell size='small' align='center'>
-                  XI
-                </TableCell>
-                <TableCell size='small' align='center'>
-                  XII
-                </TableCell>
-              </TableRow>
-            </TableHead>
-
-            <TableBody className='tableBody'>
-              <TableRow>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-              </TableRow>
+            ))}
+              
             </TableBody>
-          </Table>
-        </TableContainer>
-      </Grid>
-      <FooterReport/>
+            </Table>
+          </TableContainer>
+        </Grid>
+      )}
+
+      <FooterReport />
     </Paper>
   )
 }
@@ -147,7 +210,7 @@ const Bieumau5 = () => {
             <Grid item xs={8}>
               <Typography className='text-danger text-weight-bold'>Biểu mẫu 5</Typography>
               <Typography className='text-success text-weight-bold _font12'>
-               Dòng chảy trung bình tháng năm trong kỳ báo cáo
+                Dòng chảy trung bình tháng năm trong kỳ báo cáo
               </Typography>
             </Grid>
             <Grid item xs={4}>
--- src/views/report-form/Bieumau8.tsx
+++ src/views/report-form/Bieumau8.tsx
@@ -17,8 +17,50 @@ import DownloadIcon from '@mui/icons-material/Download'
 import DialogControlFullScreen from 'src/@core/components/dialog-control-full-screen'
 import HeaderReport from './HeaderReport'
 import FooterReport from './FooterReport'
+import { getData } from 'src/api/axios'
+import { useEffect, useState } from 'react'
+import BoxLoading from 'src/@core/components/box-loading'
+import CreateReport2 from './Bieumau2/CreateReport2'
+import DeleteData from 'src/@core/components/delete-data'
+import { CalculateReportData } from './CalculateData'
 
+interface Report1 {
+  id: number
+  tangChuaNuoc: string
+  slGieng: string
+  mucNuocMaxKyTruoc: number
+  mucNuocMaxKyBaoCao: number
+  mucNuocMediumKyTruoc: number
+  mucNuocMediumKyBaoCao: number
+  mucNuocMinKyTruoc: number
+  mucNuocMinKyBaoCao: number
+  ghiChu: string
+}
 const FormContruction = () => {
+  const [data, setData] = useState<Report1[]>([])
+  const [loading, setLoading] = useState(false)
+  const [postSuccess, setPostSuccess] = useState(false)
+  const handlePostSuccess = () => {
+    setPostSuccess(prevState => !prevState)
+  }
+  useEffect(() => {
+    async function getDataReport1() {
+      setLoading(true)
+      await getData('BieuMauSoTam/danhsach')
+        .then(data => {
+          setData(data)
+        })
+        .catch(error => {
+          console.log(error)
+        })
+        .finally(() => {
+          setLoading(false)
+        })
+    }
+
+    getDataReport1()
+  }, [postSuccess])
+
   return (
     <Paper sx={{ p: 8 }}>
       {/* dautrang */}
@@ -45,125 +87,160 @@ const FormContruction = () => {
           (Kỳ báo cáo: <TextField size='small' sx={{ width: '50px' }}></TextField>)
         </Typography>
       </Grid>
+      {loading ? (
+        <BoxLoading />
+      ) : (
+        <Grid className='_text_center' sx={{ mt: 3 }}>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+              <TableHead className='tableHead'>
+                <TableRow>
+                  <TableCell size='small' align='center' rowSpan={4}>
+                    STT
+                  </TableCell>
+                  <TableCell size='small' align='center' rowSpan={2}>
+                    Vùng/tầng <br />
+                    chứa nước
+                  </TableCell>
+                  <TableCell size='small' align='center' rowSpan={2}>
+                    Số lượng <br />
+                    giếng quan trắc
+                  </TableCell>
+                  <TableCell size='small' align='center' colSpan={3}>
+                    Mực nước lớn nhất(m)
+                  </TableCell>
+                  <TableCell size='small' align='center' colSpan={3}>
+                    Mực nước trung bình (m)
+                  </TableCell>
+                  <TableCell size='small' align='center' colSpan={3}>
+                    Mực nước nhỏ nhất (m)
+                  </TableCell>
+                  <TableCell size='small' align='center' rowSpan={4}>
+                    Thao tác
+                  </TableCell>
+                </TableRow>
 
-      <Grid className='_text_center' sx={{ mt: 3 }}>
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label='simple table'>
-            <TableHead className='tableHead'>
-              <TableRow>
-                <TableCell size='small' align='center' rowSpan={4}>
-                  STT
-                </TableCell>
-                <TableCell size='small' align='center' rowSpan={2}>
-                  Vùng/tầng <br />
-                  chứa nước
-                </TableCell>
-                <TableCell size='small' align='center' rowSpan={2}>
-                  Số lượng <br />
-                  giếng quan trắc
-                </TableCell>
-                <TableCell size='small' align='center' colSpan={3}>
-                  Mực nước lớn nhất(m)
-                </TableCell>
-                <TableCell size='small' align='center' colSpan={3}>
-                  Mực nước trung bình (m)
-                </TableCell>
-                <TableCell size='small' align='center' colSpan={3}>
-                  Mực nước nhỏ nhất (m)
-                </TableCell>
-              </TableRow>
+                <TableRow>
+                  <TableCell size='small' align='center'>
+                    Kỳ trước
+                  </TableCell>
+                  <TableCell size='small' align='center'>
+                    Kỳ báo cáo
+                  </TableCell>
+                  <TableCell size='small' align='center'>
+                    Thay đổi
+                  </TableCell>
 
-              <TableRow>
-                <TableCell size='small' align='center'>
-                  Kỳ trước
-                </TableCell>
-                <TableCell size='small' align='center'>
-                  Kỳ báo cáo
-                </TableCell>
-                <TableCell size='small' align='center'>
-                  Thay đổi
-                </TableCell>
+                  <TableCell size='small' align='center'>
+                    Kỳ trước
+                  </TableCell>
+                  <TableCell size='small' align='center'>
+                    Kỳ báo cáo
+                  </TableCell>
+                  <TableCell size='small' align='center'>
+                    Thay đổi
+                  </TableCell>
 
-                <TableCell size='small' align='center'>
-                  Kỳ trước
-                </TableCell>
-                <TableCell size='small' align='center'>
-                  Kỳ báo cáo
-                </TableCell>
-                <TableCell size='small' align='center'>
-                  Thay đổi
-                </TableCell>
+                  <TableCell size='small' align='center'>
+                    Kỳ trước
+                  </TableCell>
+                  <TableCell size='small' align='center'>
+                    Kỳ báo cáo
+                  </TableCell>
+                  <TableCell size='small' align='center'>
+                    Thay đổi
+                  </TableCell>
+                </TableRow>
 
-                <TableCell size='small' align='center'>
-                  Kỳ trước
-                </TableCell>
-                <TableCell size='small' align='center'>
-                  Kỳ báo cáo
-                </TableCell>
-                <TableCell size='small' align='center'>
-                  Thay đổi
-                </TableCell>
-              </TableRow>
+                <TableRow>
+                  <TableCell size='small' align='center'>
+                    (1)&nbsp;
+                  </TableCell>
+                  <TableCell size='small' align='center'>
+                    (2)&nbsp;
+                  </TableCell>
+                  <TableCell size='small' align='center'>
+                    (3)&nbsp;
+                  </TableCell>
 
-              <TableRow>
-                <TableCell size='small' align='center'>
-                  (1)&nbsp;
-                </TableCell>
-                <TableCell size='small' align='center'>
-                  (2)&nbsp;
-                </TableCell>
-                <TableCell size='small' align='center'>
-                  (3)&nbsp;
-                </TableCell>
+                  <TableCell size='small' align='center'>
+                    (4)
+                  </TableCell>
+                  <TableCell size='small' align='center'>
+                    (5)=(4)-(3)
+                  </TableCell>
+                  <TableCell size='small' align='center'>
+                    (6)&nbsp;
+                  </TableCell>
 
-                <TableCell size='small' align='center'>
-                  (4)
-                </TableCell>
-                <TableCell size='small' align='center'>
-                  (5)=(4)-(3)
-                </TableCell>
-                <TableCell size='small' align='center'>
-                  (6)&nbsp;
-                </TableCell>
+                  <TableCell size='small' align='center'>
+                    (7)
+                  </TableCell>
+                  <TableCell size='small' align='center'>
+                    (8)=(7)-(6)
+                  </TableCell>
+                  <TableCell size='small' align='center'>
+                    (9)
+                  </TableCell>
 
-                <TableCell size='small' align='center'>
-                  (7)
-                </TableCell>
-                <TableCell size='small' align='center'>
-                  (8)=(7)-(6)
-                </TableCell>
-                <TableCell size='small' align='center'>
-                  (9)
-                </TableCell>
+                  <TableCell size='small' align='center'>
+                    (10)
+                  </TableCell>
+                  <TableCell size='small' align='center'>
+                    (11)=(10)-(9)
+                  </TableCell>
+                </TableRow>
+              </TableHead>
 
-                <TableCell size='small' align='center'>
-                  (10)
-                </TableCell>
-                <TableCell size='small' align='center'>
-                  (11)=(10)-(9)
-                </TableCell>
-              </TableRow>
-            </TableHead>
+              <TableBody className='tableBody'>
+                {data.map((item, index) => (
+                  <TableRow key={item.id}>
+                    <TableCell className="text-center  size='small' align-middle font-13">{index + 1}</TableCell>
+                    <TableCell className="text-center  size='small' align-middle font-13">
+                      {item.tangChuaNuoc}
+                    </TableCell>
+                    <TableCell className="text-center  size='small' align-middle font-13">{item.slGieng}</TableCell>
+                    <TableCell className="text-center  size='small' align-middle font-13">
+                      {item.mucNuocMaxKyTruoc}
+                    </TableCell>
+                    <TableCell className="text-center  size='small' align-middle font-13">
+                      {item.mucNuocMaxKyBaoCao}
+                    </TableCell>
+                    <TableCell className="text-center  size='small' align-middle font-13">
+                      {CalculateReportData(item.mucNuocMaxKyBaoCao, item.mucNuocMaxKyTruoc)}
+                    </TableCell>
+                    <TableCell className="text-center  size='small' align-middle font-13">
+                      {item.mucNuocMediumKyTruoc}
+                    </TableCell>
+                    <TableCell className="text-center  size='small' align-middle font-13">
+                      {item.mucNuocMediumKyBaoCao}
+                    </TableCell>
+                    <TableCell className="text-center  size='small' align-middle font-13">
+                      {CalculateReportData(item.mucNuocMediumKyBaoCao, item.mucNuocMediumKyTruoc)}
+                    </TableCell>
+                    <TableCell className="text-center  size='small' align-middle font-13">
+                      {item.mucNuocMinKyTruoc}
+                    </TableCell>
+                    <TableCell className="text-center  size='small' align-middle font-13">
+                      {item.mucNuocMinKyBaoCao}
+                    </TableCell>
+                    <TableCell className="text-center  size='small' align-middle font-13">
+                      {CalculateReportData(item.mucNuocMinKyBaoCao, item.mucNuocMinKyTruoc)}
+                    </TableCell>
+                    <TableCell align='center' className="  size='small' align-middle font-13">
+                      <Box>
+                        <CreateReport2 isEdit={true} data={item} setPostSuccess={handlePostSuccess} />
+                        <DeleteData url={'BieuMauSoHai'} data={item} setPostSuccess={handlePostSuccess} />
+                      </Box>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+      )}
 
-            <TableBody className='tableBody'>
-              <TableRow>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-                <TableCell className="text-center  size='small' align-middle font-13">1</TableCell>
-              </TableRow>
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </Grid>
       <FooterReport />
     </Paper>
   )
