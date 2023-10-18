b234915f09ce6f3473245ca8eba289779ee5f384
NT.Dang.20 <73978541+githubnguyendang@users.noreply.github.com>
2023-10-18 19:51:27 +0700

    Update config.js

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,4 @@
 const apiUrl = 'https://api.tainguyenmoitruongquangngai.vn/api';
-
-//const apiUrl = 'https://localhost:44354/api'
+// const apiUrl = 'https://localhost:44354/api';
 
 export default apiUrl;
