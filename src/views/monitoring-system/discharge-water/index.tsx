a7d0bc6a14dfc0c9b0b748d680815f2c1dc9e2cd
Mr. Loc <kten1112@gmail.com>
2023-10-30 15:30:55 +0700

    Update monitoring system for exploit water

--- src/views/monitoring-system/discharge-water/index.tsx
+++ src/views/monitoring-system/discharge-water/index.tsx
@@ -1,186 +1,181 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 // ** MUI Imports
-import { Grid, Box, Button, Card, CardContent, IconButton, Tooltip, Typography, Autocomplete, TextField } from '@mui/material';
-
-// ** Icons Imports
-import SearchIcon from '@mui/icons-material/Search';
-import { EditNote, Delete } from "@mui/icons-material";
-
+import { Grid, Box, Typography, Paper, FormGroup, FormControlLabel, Checkbox } from '@mui/material';
 // ** Components Imports
+import MonitoringSystemToolBar from '../tool-bar';
 import TableComponent from 'src/@core/components/table';
 import DisplayOperatingStatus from 'src/@core/components/monitoring-page/check-status';
-import GroundmonitoringData from 'src/api/monitoringsystem/nuocduoidat';
+import GetConstructionTypeId from 'src/@core/components/get-construction-type';
+import { ConverterCood } from 'src/@core/components/map/convert-coord'
 
 import dynamic from 'next/dynamic';
+import { getData } from 'src/api/axios';
+import { useRouter } from 'next/router';
+import MapLegend from 'src/views/construction/MapLegend';
+import ViewMonitoringSystemData from '../form';
 
 const Map = dynamic(() => import("src/@core/components/map"), { ssr: false });
 
-const groundType = [
-  { title: 'Chọn loại CT', value: 1 },
-  { title: 'Khai thác', value: 8 },
-  { title: 'Thăm dò', value: 9 },
-  { title: 'Hành nghề khoan', value: 10 },
-  { title: 'Công trình khác', value: 23 },
-  { title: 'Trám lấp giếng', value: 24 }
-];
-
-const licensingAuthorities = [
-  { title: "BTNMT", value: 'BTNMT' },
-  { title: "UBND Tỉnh", value: 'UBNDT' },
-];
-
-
-// id of columnsTable is parameter to bind ex: get LicseFk.BasinId: id: 'License_Fk.BasinId'
-const columnsTable = [
-  { id: 'stt', label: 'STT', rowspan: 2, },
-  { id: 'ConstructionName', label: 'Tên công trình', rowspan: 2, },
-  {
-    id: '#', label: 'Lưu lượng nước thải sau xử lý', children: [
-      { id: 'MaximumFlow', label: 'Yêu cầu', },
-      { id: 'MaximumFlowPre', label: 'Thực tế ', },
-      { id: '', label: 'Chênh lệch (+/-)', },
-    ]
-  },
-  {
-    id: '#', label: 'Chất lượng nước sau xử lý', children: [
-      { id: 'Nhietdo', label: 'Nhiệt độ (°C)', },
-      { id: 'pH', label: 'pH ', },
-      { id: 'BOD5', label: 'BOD5', },
-      { id: 'COD', label: 'COD', },
-      { id: 'DO', label: 'DO', },
-      { id: 'TSS', label: 'TSS', },
-      { id: 'NH4', label: 'NH4+', },
-    ]
-  },
-  {
-    id: '#', label: 'Lưu lượng nước thải tại nguồn tiếp nhận', children: [
-      { id: 'MaximumFlow', label: 'Yêu cầu', },
-      { id: 'MaximumFlowPre', label: 'Thực tế ', },
-      { id: '', label: 'Chênh lệch (+/-)', },
-    ]
-  },
-
-  { id: '#', label: 'Trạng thái vận hành', rowspan: 2, elm: (row: any) => (<DisplayOperatingStatus data={row} />) },
-
-  { id: 'actions', label: 'Thao tác', rowspan: 2 },
-];
-
 const DischargewaterMonitoring = () => {
-  const [mapCenter] = useState([15.012172, 108.676488]);
-  const [mapZoom] = useState(9);
+  const router = useRouter();
+  const [mapCenter, setMapCenter] = useState([15.012172, 108.676488]);
+  const [mapZoom, setMapZoom] = useState(9);
+  const [showLabel, setShowLabel] = useState(false)
 
-  const [TypeOfConsId, setTypeOfConsId] = useState([1]);
-  const handleChange = (e: any) => {
-    const val = (e == undefined || e == null ? 1 : e.value)
-    setTypeOfConsId(val)
+  const [TypeOfConsId] = useState([GetConstructionTypeId(router)]);
+
+  const [resData, setResData] = useState<any[]>([]);
+  const [columns, setColumns] = useState<any[]>([]);
+  const [loading, setLoading] = useState(false)
+  const [total, setTotal] = useState(0)
+
+  const [dataFiltered, setDataFiltered] = useState([]);
+
+  const columnsTable = [
+    { id: 'stt', label: 'STT', rowspan: 2, },
+    {
+      id: 'ConstructionName', label: 'Tên công trình', rowspan: 2, elm: (row: any) => (
+        <Typography className='btnShowFilePdf' onClick={() => zoomConstruction(ConverterCood(row.y, row.x))}>
+          {row.tenCT}
+        </Typography>)
+    },
+    { id: '#', label: 'Trạng thái vận hành', rowspan: 2, elm: (row: any) => (<DisplayOperatingStatus data={row} />) },
+    {
+      id: '#', label: 'Lưu lượng nước thải sau xử lý', children: [
+        { id: 'MaximumFlow', label: 'Yêu cầu', },
+        { id: 'MaximumFlowPre', label: 'Thực tế ', },
+        { id: '', label: 'Chênh lệch (+/-)', },
+      ]
+    },
+    {
+      id: '#', label: 'Chất lượng nước sau xử lý', children: [
+        { id: 'Nhietdo', label: 'Nhiệt độ (°C)', },
+        { id: 'pH', label: 'pH ', },
+        { id: 'BOD5', label: 'BOD5', },
+        { id: 'COD', label: 'COD', },
+        { id: 'DO', label: 'DO', },
+        { id: 'TSS', label: 'TSS', },
+        { id: 'NH4', label: 'NH4+', },
+      ]
+    },
+    {
+      id: '#', label: 'Lưu lượng nước thải tại nguồn tiếp nhận', children: [
+        { id: 'MaximumFlow', label: 'Yêu cầu', },
+        { id: 'MaximumFlowPre', label: 'Thực tế ', },
+        { id: '', label: 'Chênh lệch (+/-)', },
+      ]
+    },
+    { id: 'actions', label: 'Thao tác', rowspan: 2 },
+  ];
+
+  const zoomConstruction = (coords: any) => {
+    setMapCenter(coords)
+    setMapZoom(13)
   }
 
-  const [data, setData] = useState<any[]>([]);
-  const [columns, setColumns] = useState<any[]>([]);
+  const isMounted = useRef(true);
+  useEffect(() => {
+    isMounted.current = true
+
+    return () => {
+      isMounted.current = false;
+    };
+  }, []);
 
   useEffect(() => {
-    setData(GroundmonitoringData);
+    const getDataConstructions = async () => {
+      setLoading(true);
+      getData('cong-trinh/danh-sach', paramsFilter)
+        .then((data) => {console.log(data)
+          if (isMounted.current) {
+            setResData(data);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    };
+    getDataConstructions();
+
     setColumns(columnsTable);
 
     // fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const EditLicense = (row: any) => {
-    console.log('Edit: ' + row.LicenseNumber)
-  }
+  const [paramsFilter, setParamsFilter] = useState({
+    tenct: null,
+    loai_ct: GetConstructionTypeId(router),
+    huyen: 0,
+    xa: 0,
+    song: 0,
+    luuvuc: 0,
+    tieu_luuvuc: 0,
+    tang_chuanuoc: 0,
+    tochuc_canhan: 0,
+    nguonnuoc_kt: null,
+  });
 
-  const DeleteLicense = (row: any) => {
-    console.log('Delete: ' + row.LicenseNumber)
-  }
+  const [initConsType, setInitConstype] = useState<any>([
+    'xathai', 
+    'khu_cumcn_taptrung', 
+    'sx_tieuthu_cn', 
+    'sx_kd_dichvu', 
+    'cs_benhvien', 
+    'khudancu_langnghe', 
+    'channuoi_ntts',  
+    'congtrinhkhac_xt'
+  ])
+
+  const handleFilterChange = (data: any) => {
+    setParamsFilter(data);
+  };
+
+  useEffect(() => {
+    const filteredData: any = resData.filter((item: { [key: string]: any }) =>
+      initConsType.some((keyword: any) =>
+        item['loaiCT']?.['maLoaiCT']?.toString().toLowerCase().includes(keyword.toLowerCase())
+      )
+    );
+    setDataFiltered(filteredData);console.log(filteredData);
+    setTotal(filteredData.length);
+  }, [initConsType, resData]);
+
+  const handleConsTypeChange = (data: any) => {
+    setInitConstype(data);
+  };
 
   return (
     <Grid container spacing={4}>
       <Grid item xs={12} sm={12} md={12} sx={{ height: '55vh', overflow: 'hidden' }}>
-        <Card sx={{ height: '100%' }}>
-          <CardContent sx={{ p: 0, height: '100%' }}>
-            <Map center={mapCenter} zoom={mapZoom} mapData={null} />
-          </CardContent>
-        </Card>
+        <Paper elevation={3} sx={{ height: '100%', position: 'relative' }}>
+          <Box className='map-legend' sx={{ background: 'white', pl: 2 }}>
+            <FormGroup>
+              <FormControlLabel
+                control={<Checkbox onClick={() => setShowLabel(!showLabel)} />}
+                label='Hiển thị tên công trình'
+              />
+            </FormGroup>
+            <MapLegend onChange={handleConsTypeChange} />
+          </Box>
+          <Map center={mapCenter} zoom={mapZoom} showLabel={showLabel} mapData={dataFiltered} loading={false} />
+        </Paper>
       </Grid>
       <Grid item xs={12} sm={5} md={3}>
-        <Typography>Tổng số bản ghi đã tìm thấy:132</Typography>
-      </Grid>
-      <Grid item xs={12} sm={7} md={9}>
-        <Box className='_search _row'>
-          <Grid item xs={12} sm={2} md={2}>
-            <Autocomplete
-              onChange={(e: any, v: any) => handleChange(v)}
-              size="small"
-              options={groundType}
-              getOptionLabel={(option: any) => option.title}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  variant='standard'
-                  fullWidth
-                  label="Chọn loại hình CP"
-                />
-              )}
-            />
-          </Grid>
-          <Grid item xs={12} sm={2} md={2}>
-            <Autocomplete
-              onChange={(e: any, v: any) => handleChange(v)}
-              size="small"
-              options={groundType}
-              getOptionLabel={(option: any) => option.title}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  variant='standard'
-                  fullWidth
-                  label="Chọn trạng thái kết nối"
-                />
-              )}
-            />
-          </Grid>
-          <Grid item xs={12} sm={2} md={2}>
-            <Autocomplete
-              size="small"
-              options={licensingAuthorities}
-              getOptionLabel={(option: any) => option.title}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  variant='standard'
-                  fullWidth
-                  label="Chọn cơ quan CP"
-                />
-              )}
-            />
-          </Grid>
-          <Grid item xs={12} sm={4} md={4}>
-            <TextField size='small' type='text' label='Nhập tên CT' fullWidth placeholder='' defaultValue='' />
-          </Grid>
-          <Grid item xs={12} sm={2} md={2}>
-            <Button size='small' startIcon={<SearchIcon />} variant="outlined">Tìm kiếm</Button>
-          </Grid>
-        </Box>
+        <Typography>Tổng số bản ghi đã tìm thấy: {total}</Typography>
       </Grid>
       <Grid item xs={12} sm={12} md={12}>
-        <TableComponent columns={columns} data={data} show={TypeOfConsId}
-          actions={(row: any) => (
+        <MonitoringSystemToolBar onChange={handleFilterChange} />
+        <TableComponent loading={loading} columns={columns} data={dataFiltered} show={TypeOfConsId} pagination={true}
+          actions={() => (
             <Box>
-              <Tooltip title="Chỉnh sửa giấy phép">
-                <IconButton onClick={() => EditLicense(row)}>
-                  <EditNote className='tableActionBtn' />
-                </IconButton>
-              </Tooltip>
-              <Tooltip title="Xóa giấy phép">
-                <IconButton onClick={() => DeleteLicense(row)}>
-                  <Delete className='tableActionBtn deleteBtn' />
-                </IconButton>
-              </Tooltip>
+              <ViewMonitoringSystemData />
             </Box>
-          )
-
-          } />
+          )} />
       </Grid>
     </Grid>
   )
