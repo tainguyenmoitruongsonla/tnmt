a2bb9297758a80ed33bf513bd5ea7f33192152c4
NT.Dang.20 <73978541+githubnguyendang@users.noreply.github.com>
2024-01-12 10:10:07 +0700

    Update fix error when have only 1 dashboard permission

--- src/@core/layouts/checkAccessPermission.tsx
+++ src/@core/layouts/checkAccessPermission.tsx
@@ -5,21 +5,24 @@ interface DecodedToken {
 }
 
 export const checkAccessPermission = (linkControl: string | undefined, action: string | undefined) => {
-
     if (typeof sessionStorage !== 'undefined') {
-
         const token = sessionStorage.getItem('authToken') || '';
-
         if (token) {
-            const decodedToken = jwt_decode(token) as DecodedToken;
+            const decodedToken: DecodedToken = jwt_decode(token) as DecodedToken;
 
-            const permits = decodedToken['Permission']?.map((e: any) => {
-                return JSON.parse(e);
-            })
+            let permits: any[] = [];
+            if (Array.isArray(decodedToken['Permission'])) {
+                permits = decodedToken['Permission'].map((e: any) => {
+                    return JSON.parse(e);
+                });
+            } else if (decodedToken['Permission']) {
+                // If it's not an array, assume it's a single object
+                permits = [JSON.parse(decodedToken['Permission'])];
+            }
 
             const role = decodedToken['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'];
 
-            if (role === 'Admin' || role == "Administrator") {
+            if (role === 'Admin' || role === 'Administrator') {
                 return true;
             } else {
                 // Use Array.prototype.some to check if any permit matches the condition
